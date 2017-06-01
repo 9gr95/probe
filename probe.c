21e6bdb5fbca52cdd256876b5f6168511c1a23d7
Christian Grander <christiangrander@christians-mbp.home>
2017-06-02 01:43:33 +0200

    Scanf in funktion
    Why scanf doesn’t work in this function. Can somebody help me?

---
+++ probe.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+int input(int num){
+  printf("Enter num:");
+  scanf("%d", &num);
+  return num;
+}
+
+  int main(void){
+
+
+  int num;
+
+  eingabe(num);
+  printf("%d", zahl);//Stackoverflow ??
+
+  return 0;
+  }
